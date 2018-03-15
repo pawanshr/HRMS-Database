8f2264f4af49ae0b1ccaa63feaa2b412330033c2
hbinadi <37369802+hbinadi@users.noreply.github.com>
2018-03-15 08:34:40 +0545

    Create hrms_script.sql

---
+++ hrms_script.sql
@@ -0,0 +1,25 @@
+
+-- hrms leave 
+create database seva_hrms;
+use seva_hrms;
+
+
+Create Table employee(
+emp_ID int auto_increment primary key,
+fname varchar(255) NOT NULL,
+lname varchar(255)NOT NULL,
+email varchar(255) NOT NULL,
+designation varchar(100) NOT NULL,
+project varchar(100) NOT NULL
+);
+
+
+Create table emp_leave(
+emp_id int not null,
+from_date date,
+to_date date,
+leave_type varchar(255),
+leave_status varchar(255),
+reporting_mgr varchar(255),
+Foreign key(emp_id) references employee(emp_id)
+);
